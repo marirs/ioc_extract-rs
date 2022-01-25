102d23cd07467e70d8b9e3a0648aa5a38867ce4f
marirs <marirs@gmail.com>
2022-01-25 07:59:11 +0530

    removed threading

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ pub(crate) mod validators;
 use serde::{Deserialize, Serialize};
 use std::{fs::read_to_string, io::Result, path::Path, sync::{Arc, Mutex}};
 use validators::{crypto, internet, network};
-use rayon::prelude::*;
 
 #[derive(Serialize, Deserialize, Default, Debug, Clone)]
 pub struct Indicators {
@@ -25,7 +24,7 @@ pub fn extract_from_file<P: AsRef<Path>>(file: P) -> Result<Option<Indicators>> 
     //! ```rust
     //! use ioc_extract::extract_from_file;
     //!
-    //! let f = "assets/sample.txt";
+    //! let f = "data/sample.txt";
     //! println!("{:?}", extract_from_file(f));
     //! ```
     let f = read_to_string(file)?;
@@ -42,47 +41,30 @@ pub fn extract(s: &str) -> Option<Indicators> {
     //! let x = "this is an IP address: 192.168.2.11";
     //! println!("{:?}", x);
     //! ```
-    let urls = Arc::new(Mutex::new(vec![]));
-    let domains = Arc::new(Mutex::new(vec![]));
-    let emails = Arc::new(Mutex::new(vec![]));
-    let ip_address = Arc::new(Mutex::new(vec![]));
-    let crypto_address = Arc::new(Mutex::new(vec![]));
+    let mut urls = vec![];
+    let mut domains = vec![];
+    let mut emails = vec![];
+    let mut ip_address = vec![];
+    let mut crypto_address = vec![];
     let mut _registry = vec![];
 
     // Create a default Indicator object
     let mut iocs = Indicators::default();
 
-    let all_strings = s.split_whitespace().collect::<Vec<&str>>();
-    all_strings
-        .into_par_iter()
-        .for_each(|line| {
-            if network::is_ipv_any(line) || network::is_ip_cidr_any(line) {
-                ip_address
-                    .lock()
-                    .expect("poisoned lock")
-                    .push(line.to_string());
-            } else if crypto::is_cryptocurrency_any(line) && crypto::which_cryptocurrency(line).is_some() {
-                crypto_address
-                    .lock()
-                    .expect("poisoned lock")
-                    .push(line.to_string());
-            } else if internet::is_domain(line) {
-                domains
-                    .lock()
-                    .expect("poisoned lock")
-                    .push(line.to_string());
-            } else if internet::is_url(line) {
-                urls.lock().expect("poisoned lock").push(line.to_string());
-            } else if internet::is_email(line, None) {
-                emails.lock().expect("poisoned lock").push(line.to_string());
-            }
-        });
-
-    let mut urls = urls.lock().expect("lock poisoned").to_vec();
-    let mut domains = domains.lock().expect("lock poisoned").to_vec();
-    let mut emails = emails.lock().expect("lock poisoned").to_vec();
-    let mut ip_address = ip_address.lock().expect("lock poisoned").to_vec();
-    let mut crypto_address = crypto_address.lock().expect("lock poisoned").to_vec();
+    let s = s.split_whitespace().collect::<Vec<&str>>();
+    for x in s {
+        if network::is_ipv_any(x) || network::is_ip_cidr_any(x) {
+            ip_address.push(x.to_string())
+        } else if crypto::is_cryptocurrency_any(x) {
+            crypto_address.push(x.to_string())
+        } else if internet::is_domain(x) {
+            domains.push(x.to_string())
+        } else if internet::is_url(x) {
+            urls.push(x.to_string())
+        } else if internet::is_email(x, None) {
+            emails.push(x.to_string())
+        }
+    }
 
     if urls.is_empty()
         && domains.is_empty()
