b1e57481ba3d145e931a951eab4093993f9d8392
marirs <marirs@gmail.com>
2022-01-31 09:31:34 +0530

    api change

--- examples/xtract.rs
+++ examples/xtract.rs
@@ -1,8 +1,8 @@
-use ioc_extract::from_str;
+use ioc_extract::Artifacts;
 
 fn main() {
     let x = "there are ips in this test\n192.168.21.21 and ::ffff:127.0.0.1\nthe cidrs are:\n2001:0DB8:1234::/48 and \n10.0.0.0/8\n\n";
     let x = x.to_owned() + "check out https://www.google.com or www.google.com";
-    let ioc = from_str(&x);
+    let ioc = Artifacts::from_str(&x);
     println!("IOC's:\n{:#?}", ioc);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -28,124 +28,126 @@ pub struct Artifacts {
     pub regexes: Option<Vec<String>>,
 }
 
-pub fn from_file<P: AsRef<Path>>(file: P) -> Result<Option<Artifacts>> {
-    //! Extracts Indicators from a given file
-    //!
-    //! ## Example Usage
-    //! ```rust
-    //! use ioc_extract::from_file;
-    //!
-    //! let f = "data/sample.txt";
-    //! println!("{:?}", from_file(f));
-    //! ```
-    let f = read_to_string(file)?;
-    Ok(from_str(&f))
-}
+impl Artifacts {
+    pub fn from_file<P: AsRef<Path>>(file: P) -> Result<Option<Self>> {
+        //! Extracts Indicators from a given file
+        //!
+        //! ## Example Usage
+        //! ```rust
+        //! use ioc_extract::Artifacts;
+        //!
+        //! let f = "data/sample.txt";
+        //! println!("{:?}", Artifacts::from_file(f));
+        //! ```
+        let f = read_to_string(file)?;
+        Ok(Self::from_str(&f))
+    }
 
-pub fn from_str(s: &str) -> Option<Artifacts> {
-    //! Extracts Indicators from a given string
-    //!
-    //! ## Example Usage
-    //! ```rust
-    //! use ioc_extract::from_str;
-    //!
-    //! let x = "this is an IP address: 192.168.2.11";
-    //! println!("{:?}", x);
-    //! ```
-    let mut urls = vec![];
-    let mut domains = vec![];
-    let mut emails = vec![];
-    let mut ip_address = vec![];
-    let mut crypto_address = vec![];
-    let mut registry = vec![];
-    let mut sql = vec![];
-    let mut regexes = vec![];
+    pub fn from_str(s: &str) -> Option<Self> {
+        //! Extracts Indicators from a given string
+        //!
+        //! ## Example Usage
+        //! ```rust
+        //! use ioc_extract::Artifacts;
+        //!
+        //! let x = "this is an IP address: 192.168.2.11";
+        //! println!("{:?}", Artifacts::from_str(x));
+        //! ```
+        let mut urls = vec![];
+        let mut domains = vec![];
+        let mut emails = vec![];
+        let mut ip_address = vec![];
+        let mut crypto_address = vec![];
+        let mut registry = vec![];
+        let mut sql = vec![];
+        let mut regexes = vec![];
 
-    // Create a default Indicator object
-    let mut iocs = Artifacts::default();
+        // Create a default Indicator object
+        let mut iocs = Artifacts::default();
 
-    // check for registry keys & sql queries by breaking only newlines
-    for x in s.split('\n').collect::<Vec<&str>>() {
-        if system::is_registry_key(x) {
-            registry.push(x.to_string())
-        } else if system::is_sql(x) {
-            sql.push(x.to_string())
+        // check for registry keys & sql queries by breaking only newlines
+        for x in s.split('\n').collect::<Vec<&str>>() {
+            if system::is_registry_key(x) {
+                registry.push(x.to_string())
+            } else if system::is_sql(x) {
+                sql.push(x.to_string())
+            }
         }
-    }
 
-    // check for the rest by breaking newlines, whitespace, tabs, etc...
-    for x in s.split_whitespace().collect::<Vec<&str>>() {
-        if network::is_ipv_any(x) || network::is_ip_cidr_any(x) {
-            ip_address.push(x.to_string())
-        } else if crypto::is_cryptocurrency_any(x) {
-            crypto_address.push(x.to_string())
-        } else if internet::is_domain(x) {
-            domains.push(x.to_string())
-        } else if internet::is_url(x) {
-            urls.push(x.to_string())
-        } else if internet::is_email(x, None) {
-            emails.push(x.to_string())
-        } else if system::is_regex(x) {
-            regexes.push(x.to_string())
+        // check for the rest by breaking newlines, whitespace, tabs, etc...
+        for x in s.split_whitespace().collect::<Vec<&str>>() {
+            if network::is_ipv_any(x) || network::is_ip_cidr_any(x) {
+                ip_address.push(x.to_string())
+            } else if crypto::is_cryptocurrency_any(x) {
+                crypto_address.push(x.to_string())
+            } else if internet::is_domain(x) {
+                domains.push(x.to_string())
+            } else if internet::is_url(x) {
+                urls.push(x.to_string())
+            } else if internet::is_email(x, None) {
+                emails.push(x.to_string())
+            } else if system::is_regex(x) {
+                regexes.push(x.to_string())
+            }
         }
-    }
 
-    if urls.is_empty()
-        && domains.is_empty()
-        && emails.is_empty()
-        && ip_address.is_empty()
-        && crypto_address.is_empty()
-        && registry.is_empty()
-        && sql.is_empty()
-        && regexes.is_empty()
-    {
-        return None;
-    }
+        if urls.is_empty()
+            && domains.is_empty()
+            && emails.is_empty()
+            && ip_address.is_empty()
+            && crypto_address.is_empty()
+            && registry.is_empty()
+            && sql.is_empty()
+            && regexes.is_empty()
+        {
+            return None;
+        }
 
-    if !urls.is_empty() {
-        urls.sort();
-        urls.dedup();
-        iocs.urls = Some(urls);
-    }
-    if !domains.is_empty() {
-        domains.sort();
-        domains.dedup();
-        iocs.domains = Some(domains);
-    }
-    if !emails.is_empty() {
-        emails.sort();
-        emails.dedup();
-        iocs.emails = Some(emails);
-    }
-    if !ip_address.is_empty() {
-        ip_address.sort();
-        ip_address.dedup();
-        iocs.ip_address = Some(ip_address);
-    }
-    if !crypto_address.is_empty() {
-        crypto_address.sort();
-        crypto_address.dedup();
-        iocs.crypto = Some(crypto_address);
-    }
-    if !registry.is_empty() {
-        registry.sort();
-        registry.dedup();
-        iocs.registry_keys = Some(registry);
-    }
-    if !sql.is_empty() {
-        sql.sort();
-        sql.dedup();
-        iocs.sql = Some(sql);
-    }
+        if !urls.is_empty() {
+            urls.sort();
+            urls.dedup();
+            iocs.urls = Some(urls);
+        }
+        if !domains.is_empty() {
+            domains.sort();
+            domains.dedup();
+            iocs.domains = Some(domains);
+        }
+        if !emails.is_empty() {
+            emails.sort();
+            emails.dedup();
+            iocs.emails = Some(emails);
+        }
+        if !ip_address.is_empty() {
+            ip_address.sort();
+            ip_address.dedup();
+            iocs.ip_address = Some(ip_address);
+        }
+        if !crypto_address.is_empty() {
+            crypto_address.sort();
+            crypto_address.dedup();
+            iocs.crypto = Some(crypto_address);
+        }
+        if !registry.is_empty() {
+            registry.sort();
+            registry.dedup();
+            iocs.registry_keys = Some(registry);
+        }
+        if !sql.is_empty() {
+            sql.sort();
+            sql.dedup();
+            iocs.sql = Some(sql);
+        }
 
-    if !regexes.is_empty() {
-        regexes.sort();
-        regexes.dedup();
-        iocs.regexes = Some(regexes);
-    }
+        if !regexes.is_empty() {
+            regexes.sort();
+            regexes.dedup();
+            iocs.regexes = Some(regexes);
+        }
 
-    // return the result object
-    Some(iocs)
+        // return the result object
+        Some(iocs)
+    }
 }
 
 #[cfg(test)]
@@ -155,7 +157,7 @@ mod tests {
     #[test]
     fn test_extract_ip() {
         let x = "there are ips in this test\n192.168.21.21 and ::ffff:127.0.0.1\nthe cidrs are:\n2001:0DB8:1234::/48 and \n10.0.0.0/33";
-        let ioc = from_str(x);
+        let ioc = Artifacts::from_str(x);
         assert!(ioc.is_some());
         let ips = ioc.unwrap().ip_address;
         assert!(ips.is_some())
